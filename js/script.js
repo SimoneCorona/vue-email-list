91c2fc9765cf0ad0348c2993c9885174b0c8604f
SimoneCorona <corona.simone93@gmail.com>
2022-05-13 16:20:57 +0200

    empty array

---
+++ js/script.js
@@ -0,0 +1,28 @@
+// Attraverso l'apposita API di Boolean
+// https://flynn.boolean.careers/exercises/api/random/mail
+// generare 10 indirizzi email e stamparli in pagina all'interno di una lista.
+// Bonus
+// Far comparire gli indirizzi email solamente quando sono stati tutti generati.
+
+const app = new Vue(
+    {
+        el: '#root',
+        data: {
+            email: null,
+            emailsArray: [],
+        },
+        created() {
+            axios.get('https://flynn.boolean.careers/exercises/api/random/mail', {  
+            })
+            .then((resp) => {
+                this.email = resp.data.response;
+            });
+            createArrayEmail= () => {
+                while (this.emailsArray.length < 10) {
+                    this.emailsArray.push(this.email)
+                }
+            }
+        },
+    }
+) 
+
